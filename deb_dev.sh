14010ea17d33dd18e96193fa5c0135af6804bba5
Bozarr <111038696+Bozarr@users.noreply.github.com>
2022-08-22 12:58:19 +0500

    neovim firefox git

--- deb_dev.sh
+++ deb_dev.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+
+# git
+exec apt install git -y
+
+# nvim
+exec apt install neovim -y
+
+# firefox
+exec apt install firefox-esr -y
+
+# fish shell
 exec apt install fish -y
+usermod -s $(which fish) username
