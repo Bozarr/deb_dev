f308fdf4a4857721c0d8df43b00611c3d99f2230
Bozarr <111038696+Bozarr@users.noreply.github.com>
2022-08-23 13:49:40 +0500

    fix pycharm, add comments

--- deb_dev.sh
+++ deb_dev.sh
@@ -7,16 +7,27 @@ exec apt install \
   neovim \
   firefox-esr \
   fish \
-  xserver-xorg xinit \
+  xserver-xorg x11-xserver-utils xinit \
   libpangocairo-1.0-0 \
+  # remove this and install python from source?
   python3-pip python3-xcffib python3-cairocffi \
   htop \  
   -y
 
 exec pip install qtile
 
+# configure user
 exec usermod -aG sudo usr
 exec usermod -s $(which fish) usr
 
+# install pycharm
+# see https://www.jetbrains.com/help/pycharm/installation-guide.html#33d56af2 for more info
 link=https://download.jetbrains.com/python/pycharm-community-2022.2.1.exe
-wget $link  # is wget avaliable on clear system?
+# tar xzf pycharm-*.tar.gz -C <new_archive_folder> --- with output option
+wget $link # is wget avaliable on clear system?
+# tar xzf pycharm-*.tar.gz -C <new_folder> --- with output option
+tar xzf pycharm-*.tar.gz
+rm pycharm-community-2022.2.1
+
+# bind pycharm execution to qtile config Key([mod], "p", lazy.spawn("pycharm_directory/bin/pycharm.sh"))
+# bind firefox execution to qtile config Key([mod], "f", lazy.spawn(firefox))
