424cfff18b8588a46c6affffc4f68a580801ac91
Bozarr <111038696+Bozarr@users.noreply.github.com>
2022-08-23 11:23:22 +0500

    download pycharm; add usr to sudoers

--- deb_dev.sh
+++ deb_dev.sh
@@ -2,15 +2,21 @@
 
 # INSTALL
 exec apt install \
+  sudo \
+  curl \
   neovim \
   firefox-esr \
   fish \
   xserver-xorg xinit \
   libpangocairo-1.0-0 \
   python3-pip python3-xcffib python3-cairocffi \
-  htop \
-  
+  htop \  
   -y
 
 exec pip install qtile
-exec usermod -s $(which fish) username
+
+exec usermod -aG sudo usr
+exec usermod -s $(which fish) usr
+
+link=https://download.jetbrains.com/python/pycharm-community-2022.2.1.exe
+wget $link  # is wget avaliable on clear system?
