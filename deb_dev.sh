c5355d6e92bf45d9847d0b57d736959d06f43ade
Bozarr <111038696+Bozarr@users.noreply.github.com>
2022-08-23 16:08:50 +0500

    install fresh python, remove some dependenies
    add deps for qtile

--- deb_dev.sh
+++ deb_dev.sh
@@ -1,30 +1,42 @@
 #!/bin/bash
+# preparations
+apt update
+apt upgrade
 
 # INSTALL
-exec apt install \
+# python dependencies
+apt install -y \
   sudo \
+  make \
+  wget \
   curl \
   neovim \
   firefox-esr \
   fish \
   xserver-xorg x11-xserver-utils xinit \
-  libpangocairo-1.0-0 \
-  # remove this and install python from source?
-  python3-pip python3-xcffib python3-cairocffi \
-  htop \  
-  -y
+  htop \
+  tmux \
+
+# python
+wget https://www.python.org/ftp/python/3.10.6/Python-3.10.6.tgz
+tar xzf Python-*.tgz
+rm Python-*.tgz
+
+echo Finished python installation
+
+echo Finished tools installation
 
 exec pip install qtile
 
 # configure user
-exec usermod -aG sudo usr
-exec usermod -s $(which fish) usr
+exec usermod -aG sudo uur
+exec usermod -s $(which fish) uur
 
 # install pycharm
 # see https://www.jetbrains.com/help/pycharm/installation-guide.html#33d56af2 for more info
 link=https://download.jetbrains.com/python/pycharm-community-2022.2.1.exe
 # tar xzf pycharm-*.tar.gz -C <new_archive_folder> --- with output option
-wget $link # is wget avaliable on clear system?
+wget $link
 # tar xzf pycharm-*.tar.gz -C <new_folder> --- with output option
 tar xzf pycharm-*.tar.gz
 rm pycharm-community-2022.2.1
